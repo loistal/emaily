b6d10de7cfa0255cdc52ad19052cf20eb92310f7
Lois Talagrand <lois.talagrand@gmail.com>
2020-01-01 14:10:32 -1000

    Server structure refactor

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   googleClientID:
-    "79826061069-nn2hi39v4v60hcso78kcscdjlm9eq7ls.apps.googleusercontent.com",
-  googleClientSecret: "ZakYzCWgYaZGdOuxz4BBwJIo"
+    "79826061069-d1julsgrocav4irio8t9tlv22qcshfma.apps.googleusercontent.com",
+  googleClientSecret: "tSEfTdW1X2fsx6VthOfzNvFF"
 };
