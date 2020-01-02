8ad0d76ef3872dde2175360a914f4d0cabfb65b1
Lois Talagrand <lois.talagrand@gmail.com>
2020-01-02 07:56:40 -1000

    Handle cookies

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,18 @@ const mongoose = require("mongoose");
 
 const User = mongoose.model("users");
 
+passport.serializeUser((user, done) => {
+  // user.id refers to the ID given by mongoDB, not by google
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => {
+      done(null, user);
+    });
+})
+
 passport.use(
   new GoogleStrategy(
     {
@@ -12,7 +24,17 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback'
     }, (accessToken, refreshToken, profile, done) => {
-      new User({ googleID: profile.id }).save();
+      User.findOne({ googleID : profile.id })
+        .then((existingUser) => {
+          if (existingUser) {
+            // already have a record with the same profile ID
+            done(null, existingUser);
+          } else {
+            // no user with this ID
+            new User({ googleID: profile.id }).save()
+              .then(user => done(null, user));
+          }
+        });
     }
   )
 );
