cccb05375686f0486f5d55dec52cf216f277240b
Lois Talagrand <lois.talagrand@gmail.com>
2020-01-01 15:54:57 -1000

    Ingore api key

--- config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  googleClientID:
-    "79826061069-d1julsgrocav4irio8t9tlv22qcshfma.apps.googleusercontent.com",
-  googleClientSecret: "tSEfTdW1X2fsx6VthOfzNvFF",
-  mongoURI: "mongodb+srv://loistal:AuthorityHacker10000@emaily-tfghj.mongodb.net/test?retryWrites=true&w=majority"
-};
